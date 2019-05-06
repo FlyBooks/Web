a122af414553f50631c1e85e08ce92b85ca01bc8
FlyBooks <2271438170@qq.com>
2019-05-06 09:15:30 +0800

    additional path optimize

--- js/myAjax.js
+++ js/myAjax.js
@@ -1,13 +1,23 @@
-function myAJAX(url, success, error) {
+function getStr(obj){
+	var formatvalue=[];
+	obj.t = new Date().getTime();
+	for(var key in obj){
+		formatvalue.push(key+'='+obj[key]);
+	}
+	return formatvalue.join('&');
+}
+
+function myAJAX(url,obj, success, error) {
 	// step1 创建异步对象
 	var xhr;
+	var additionalpath = getStr(obj);
 	if (window.XMLHttpRequest) {
 		xhr = new XMLHttpRequest();
 	} else {
 		xhr = new ActiveXObject("Microsoft.XMLHTTP"); // for IE
 	}
 	// step3 设置请求方式跟地址
-	xhr.open("GET", url + "?t=" + (new Date().getTime()), true);
+	xhr.open("GET", url + "?" +additionalpath);
 	xhr.send();
 	// step4 监听状态
 	xhr.onreadystatechange = function(ev2) {
